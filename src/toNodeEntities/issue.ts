83979718e1f8ed967e0089c205def81a61f7ac4f
Daniil <48389713+Veserion@users.noreply.github.com>
2020-12-15 00:23:25 +0300

    fix Issue tx (#11)
    fix decimals in Issue tx

--- src/toNodeEntities/issue.ts
+++ src/toNodeEntities/issue.ts
@@ -1,29 +1,30 @@
-import { TYPES } from '../constants';
-import { IIssueTransaction } from '@waves/ts-types';
-import { factory } from '../core/factory';
-import { TLong, TWithPartialFee } from '../types';
-import { getDefaultTransform, IDefaultGuiTx } from './general';
-import { getCoins, pipe, prop } from '../utils';
-
-
+import {TYPES} from '../constants';
+import {IIssueTransaction} from '@waves/ts-types';
+import {factory} from '../core/factory';
+import {TLong, TWithPartialFee} from '../types';
+import {getDefaultTransform, IDefaultGuiTx} from './general';
+import {getCoins, pipe, prop} from '../utils';
 
 export const issue = factory<IWavesGuiIssue, TWithPartialFee<IIssueTransaction<string>>>({
     ...getDefaultTransform(),
     name: prop('name'),
     description: prop('description'),
-    decimals: prop('precision'),
+    decimals: (data) => prop('decimals', data) || prop('precision', data) || 0,
     quantity: pipe<IWavesGuiIssue, TLong, string>(prop('quantity'), getCoins),
     reissuable: prop('reissuable'),
     chainId: prop('chainId'),
     script: prop('script'),
 });
 
-export interface IWavesGuiIssue extends IDefaultGuiTx<typeof TYPES.ISSUE> {
+interface IIssue extends IDefaultGuiTx<typeof TYPES.ISSUE> {
     name: string;
     description: string;
-    precision: number;
     quantity: TLong;
+    precision?: number;
+    decimals?: number;
     reissuable: boolean;
     chainId: number;
     script?: string | null;
 }
+
+export type IWavesGuiIssue = IIssue & { decimals: number } | IIssue & { precision: number };
